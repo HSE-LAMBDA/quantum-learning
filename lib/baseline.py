f4fa91e11572fb845cc477375788125d25800f6d
Sergey Popov <popovsergey95@gmail.com>
2019-12-18 16:29:52 +0300

    rmsprop

--- lib/baseline.py
+++ lib/baseline.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 class BaselineModel:
-    def __init__(self, data, target, learning_rate=1e-1):
+    def __init__(self, data, target, learning_rate=1e-3):
         dim = int(data.get_shape()[-1])
         M_real, M_img = tf.Variable(tf.random.normal([dim, dim])), tf.Variable(tf.random.normal([dim, dim]))
         M = tf.complex(M_real, M_img)
@@ -12,7 +12,7 @@ class BaselineModel:
 
         prediction = tf.math.real(tf.linalg.trace(tf.einsum('bij,jk->bik', data, self._sigma)))
         self._loss = tf.compat.v1.losses.mean_squared_error(prediction, target)
-        self._optimize = tf.compat.v1.train.GradientDescentOptimizer(
+        self._optimize = tf.compat.v1.train.RMSPropOptimizer(
             learning_rate=learning_rate).minimize(self._loss, var_list=[M_real, M_img])
 
     @property
