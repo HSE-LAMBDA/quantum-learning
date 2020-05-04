fca1ca10a70b2d3ca2632afb72beca918271d4a6
Sergei Popov <popovsergey95@gmail.com>
2020-05-04 17:53:42 +0300

    update offline learning

--- scripts/offline_learning.py
+++ scripts/offline_learning.py
@@ -1,90 +1,95 @@
 import os
 import sys
 import time
+import random
 import argparse
 import numpy as np
 import tensorflow as tf
 from itertools import count
-from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 ROOT_PATH = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-2])
 sys.path.insert(0, ROOT_PATH)
 import lib
 
+def trace(tensor):
+    if len(tensor.shape) == 2:
+        return sum([tensor[i,i] for i in range(tensor.shape[0])])
+    if len(tensor.shape) == 3:
+        return sum([tensor[:, i,i] for i in range(tensor.shape[1])])
 
-def init_writer(args):
-    current_time = '_{}.{:0>2d}.{:0>2d}_{:0>2d}:{:0>2d}:{:0>2d}'.format(*time.gmtime()[:6])
-    log_dir = os.path.join(ROOT_PATH, './logs', args.exp_name + current_time)
-    writer = SummaryWriter(log_dir, comment=args.exp_name)
-    writer.add_text('params', ','.join(map(str, [args.exp_name, args.n_qubits, args.n_proj,
-                                                 args.n_measur, args.lr, args.seed])))
-    return writer
+def metrics(sigma, X, y):
+    return np.mean(np.real((trace(X.dot(sigma)))-y)**2)
 
-
-def train_model(rho, args):
+def train_model(true_state, args):
     dim = 2 ** args.n_qubits
+    valid_proj = 10000
+    valid_meas = 10000
     x_ph = tf.placeholder(dtype=tf.complex64, shape=[None, dim, dim])
     y_ph = tf.placeholder(dtype=tf.float32, shape=[None])
     model = lib.BaselineModel(x_ph, y_ph, learning_rate=args.lr)
 
-    X_train, y_train = lib.generate_dataset(rho, args.n_proj, args.n_measur, noise=args.noise)
-    y_train = y_train.astype('float64')
-    measurements_rho = np.array([lib.simulator.bornRule(x, rho) for x in X_train])
-
+    train_X, train_y = lib.generate_dataset(
+        true_state, parsed_args.n_projectors, parsed_args.n_measurements)
+    train_y = train_y.astype('float64')
+    valid_X, valid_y = lib.generate_dataset(true_state, valid_proj, valid_meas)
+    valid_y = valid_y.astype('float64')
+    
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
-    writer = init_writer(args)
-    fidelity_history = []
-    loss_history = []
+    sigma = sess.run(model.sigma)
+    start_loss = metrics(sigma, train_X, train_y)
+    start_valid = metrics(sigma, valid_X, valid_y)
+    tolerance = 10e-5 * start_loss
+
+    last_loss = 0
     for t in tqdm(count()):
-        indices = np.random.permutation(args.n_proj)[:args.batch_size]
-        loss_t, _ = sess.run([model.loss, model.optimize], {x_ph: X_train[indices], y_ph: y_train[indices]})
-        loss_history.append(loss_t)
-        writer.add_scalar('train/loss', loss_t, t)
+        indices = np.random.permutation(args.n_projectors)[:args.batch_size]
+        loss_t, _ = sess.run([model.loss, model.optimize], {x_ph: train_X[indices], y_ph: train_y[indices]})
 
-        if t % 25 == 0:
+        if (t + 1) % 100 == 0:
             sigma = sess.run(model.sigma)
-            measurements_sigma = np.array([lib.simulator.bornRule(x, sigma) for x in X_train])
-
-            diff = abs(measurements_sigma - measurements_rho)
-            fidelity = lib.fidelity(rho, sigma)
-            writer.add_scalar('valid/metrics', diff.mean(), t)
-            writer.add_scalar('valid/fidelity', fidelity, t)
-            fidelity_history.append(fidelity)
-            if t > 0 and abs(loss_history[-1] - loss_history[-2]) < 1e-10:
+            final_loss = metrics(sigma, train_X, train_y)
+            if abs(last_loss - loss_t) < tolerance:
                 break
+        last_loss = loss_t
 
-    return sess.run(model.sigma), fidelity_history[-1], loss_history[-1]
+    final_valid = metrics(sigma, valid_X, valid_y)
+    fidelity = lib.fidelity(true_state, sigma)
+    return sigma, (fidelity, start_loss, final_loss, start_valid, final_valid)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--n-qubits', type=int, required=True)
-    parser.add_argument('--n-proj', type=int, required=True)
-    parser.add_argument('--n-measur', type=int, required=True)
+    parser.add_argument('--n-projectors', type=int, required=True)
+    parser.add_argument('--n-measurements', type=int, required=True)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--noise', type=float, default=0)
     parser.add_argument('--batch-size', type=int, default=5000)
     parser.add_argument('--exp-name', type=str, default='untitled')
     parser.add_argument('--seed', type=int, default=42)
     parsed_args = parser.parse_args()
+
     np.random.seed(parsed_args.seed)
     tf.set_random_seed(parsed_args.seed)
+    random.seed(parsed_args.seed)
 
     t = time.time()
     true_state = lib.randomMixedState(2 ** parsed_args.n_qubits)
-    pred_state, fidelity, loss = train_model(true_state, parsed_args)
+    pred_state, results = train_model(true_state, parsed_args)
+    fidelity, start_loss, final_loss, start_valid, final_valid = results
 
-    results_file = open('logs/results', 'a', buffering=1)
     report = {
         'exp': parsed_args.exp_name,
         'qubits': parsed_args.n_qubits,
-        'proj': parsed_args.n_proj,
-        'meas': parsed_args.n_measur,
+        'projectors': parsed_args.n_projectors,
+        'measurements': parsed_args.n_measurements,
         'noise': parsed_args.noise,
-        'fidel': fidelity,
-        'loss': loss,
+        'fidelity': fidelity,
+        'start_loss': start_loss,
+        'final_loss': final_loss,
+        'start_valid': start_valid,
+        'final_valid': final_valid,
         'time': time.time() - t,
     }
-    results_file.write(str(report) + '\n')
-    results_file.close()
+    print(report)
